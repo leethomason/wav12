a51cae5b6d9ea2b79e33a1155a1adc23603d8b97
Lee Thomason <leethomason@gmail.com>
2018-09-22 18:57:32 -0700

    tweak algorithm to handle big jumps

--- wav12/compress.cpp
+++ wav12/compress.cpp
@@ -2,20 +2,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <limits.h>
 #include "bits.h"
+
+static const int32_t A = 1;
+static const int32_t B = 1;
 
 void linearCompress(const int16_t* data, int32_t nSamples, 
     uint8_t** compressed, int32_t* nCompressed, 
     int shiftBits,
-    int* buckets)
+    CompressStat* stats)
 {
-    *compressed = new uint8_t[nSamples * 2];
-    BitWriter writer(*compressed, nSamples * 2);
+    static const int SIZE = nSamples * 3;
+    *compressed = new uint8_t[SIZE];
+    BitWriter writer(*compressed, SIZE);
 
     int16_t prev2 = 0;
     int16_t prev1 = 0;
     for (int i = 0; i < nSamples; i++) {
-        int32_t guess = prev1 + (prev1 - prev2);
+        int32_t guess = prev1 + (prev1 - prev2) * A / B;
         int32_t sample = data[i] >> shiftBits;
         int32_t delta = sample - guess;
         int sign = 1;
@@ -25,13 +30,26 @@ void linearCompress(const int16_t* data, int32_t nSamples,
         }
 
         int bits = BitAccum::bitsNeeded(delta);
-        assert(bits > 0 && bits <= 15);
-        writer.write(bits, 4);
-        writer.write(sign, 1);
-        writer.write(delta, bits);
+        assert(bits > 0);
+        if (bits > 15) {
+            // Edge case: it's possible to have a delta that needs 16 bits OR
+            // the guess has gone "out of range". In either case, all bits set
+            // indicates just read a value, not a delta.
+            writer.write(15, 4);
+            writer.write(uint16_t(sample), 16);
 
-        if (buckets) {
-            buckets[bits] += 1;
+            if (stats) {
+                stats->edgeWrites += 1;
+            }
+        }
+        else {
+            writer.write(bits - 1, 4); // Bits can be [1, 15], write out [0, 14]
+            writer.write(sign, 1);
+            writer.write(delta, bits);
+
+            if (stats) {
+                stats->buckets[bits - 1] += 1;
+            }
         }
 
         prev2 = prev1;
@@ -51,18 +69,39 @@ void linearExpand(const uint8_t* compressed, int nCompressed,
     int16_t prev2 = 0;
     int16_t prev1 = 0;
     for (int i = 0; i < nSamples; ++i) {
-        int32_t guess = prev1 + (prev1 - prev2);
+        int32_t guess = prev1 + (prev1 - prev2) * A / B;
         uint32_t nBits = reader.read(4);
-        uint32_t sign = reader.read(1);
-        uint32_t error = reader.read(nBits);
+        int16_t sample = 0;
+        if (nBits == 15) {
+            sample = int16_t(reader.read(16));
+        }
+        else {
+            nBits++;
+            assert(nBits > 0 && nBits < 16);
+            uint32_t sign = reader.read(1);
+            uint32_t error = reader.read(nBits);
 
-        int16_t delta = int16_t(error) * (sign == 1 ? 1 : -1);
-        int16_t sample = guess + delta;
-
+            int16_t delta = int16_t(error) * (sign == 1 ? 1 : -1);
+            sample = guess + delta;
+        }
         data[i] = sample << shiftBits;
 
         prev2 = prev1;
         prev1 = sample;
     }
 }
+
+
+void CompressStat::consolePrint() const
+{
+    for (int b = 0; b < 16; ++b) {
+        printf("bucket[%02d][%6d - %6d)=%d\n", 
+            b,
+            b == 0 ? 0 : (1 << b),
+            (1 << (b+1)),
+            buckets[b]);
+    }
+    printf("edgeWrites=%d\n", edgeWrites);
+}
+
 
--- wav12/compress.h
+++ wav12/compress.h
@@ -3,10 +3,21 @@
 
 #include <stdint.h>
 
+struct CompressStat
+{
+    // 0: 0-1
+    // 1: 2-3
+    // 2: 4-7 etc.
+    int buckets[16] = { 0 };
+    int edgeWrites = 0;
+
+    void consolePrint() const;
+};
+
 void linearCompress(const int16_t* data, int32_t nSamples,
     uint8_t** compressed, int32_t* nCompressed,
     int shiftBits = 0,
-    int* buckets = 0);
+    CompressStat* stats = 0);
 
 void linearExpand(const uint8_t* compressed, int nCompressed,
     int16_t* data, int32_t nSamples,
