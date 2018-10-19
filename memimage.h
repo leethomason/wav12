95bc9c7c16c20dda7f29232e45463cdf0224dbb5
Lee Thomason <leethomason@gmail.com>
2018-10-18 21:42:40 -0700

    setting up memory image

---
+++ memimage.h
@@ -0,0 +1,33 @@
+#ifndef MEMORY_IMAGE_INCLUDE
+#define MEMORY_IMAGE_INCLUDE
+
+#include <stdint.h>
+
+/*
+    Directory (16 bytes):
+    8 bytes:     name
+    uint32_t:    offset
+    uint32_t:    count
+
+    1k:
+    4 directories
+    60 files
+*/
+
+struct MemUnit {
+    static const int NAME_LEN = 8;
+
+    char name[NAME_LEN];   // NOT null terminated.
+    uint32_t offset;
+    uint32_t size;
+};
+
+struct MemImage {
+    static const int NUM_DIR = 4;
+    static const int NUM_FILES = 60;
+
+    MemUnit dir[NUM_DIR];
+    MemUnit file[NUM_FILES];
+};
+
+#endif // MEMORY_IMAGE_INCLUDE
